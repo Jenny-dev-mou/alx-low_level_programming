936e9bc14820c8fa17bb9840531148f368934cf2
Jenny-dev-mou <jennifermourice15@gmail.com>
2023-06-19 07:16:19 -0700

    changes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  * main - prints the phrase with proper grammar,
  *
@@ -9,12 +7,8 @@
  * Return: 0 if exited properly, non-zero otherwise
  */
 
-int main(void)
-  
-{
-  
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  
-  return (0);
-  
+int main(void) 
+{  
+printf("with proper grammar, but the outcome is a piece of art,\n"); 
+return (0);  
 }
