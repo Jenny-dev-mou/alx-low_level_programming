ed38436673da7b20c9bb3083c44c2a1b57cd95d9
Jenny-dev-mou <jennifermourice15@gmail.com>
2023-06-20 06:30:11 -0700

    main.h file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,23 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: prints '_putchar \n'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char str[] = "_putchar\n";
+int i = 0;
+while (str[i] != '\0')
+	{
+	char c = str[i];
+
+	_putchar(c);
+	i++;
+	}
+return (0);
+}#include <stdio.h>
+//*
 /*print putchar*/
 int main(void)
 {
