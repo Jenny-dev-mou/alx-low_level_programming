d668f3dfcdbfab63d3a237438a28098d3627641a
Jenny-dev-mou <jennifermourice15@gmail.com>
2023-06-21 12:56:27 -0700

    0-main.c, main.h

---
+++ 0-main.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * main tset function for positive or nagative
+ * return 0
+ */
+
+int main(void)
+{
+int i;
+	i=0;
+	positive_or_nagative(i);
+return(0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-0x00-hello_world
+0x03. C - Debugging
+
