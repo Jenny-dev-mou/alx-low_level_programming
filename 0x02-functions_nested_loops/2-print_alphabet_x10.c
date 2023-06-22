031a81a3fd806c1d7ed4e52e089a5bac183458c6
Jenny-dev-mou <jennifermourice15@gmail.com>
2023-06-22 10:23:31 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,31 @@
  */
 void print_alphabet_x10(void)
 {
-  char ch;  
-  int i;  
-  i = 0;  
-  while (i < 10)
-    {
-      ch = 'a'; 
-      while (ch <= 'z')
-	{
-	  _putchar(ch); 
-	  ch++;
+	char ch;
+
+        int i;
+
+	i = 0;
+
+        while (i < 10)
+
+        {
+
+		ch = 'a';
+
+                while (ch <= 'z')
+
+                {
+
+ 			_putchar(ch);
+
+                        ch++;
+
+		}
+		_putchar('\n');
+
+		i++;
+
 	}
-      _putchar('\n');
-      i++; 
-    }
+
 }
