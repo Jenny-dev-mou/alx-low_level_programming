ea2d54ee579a17897c091662c7a1b09feb4b4ee2
Jenny-dev-mou <jennifermourice15@gmail.com>
2023-07-13 14:59:03 -0700

    changes

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,9 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 #define ERR_MSG "Error"
+
 /**
  * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
@@ -11,14 +13,16 @@
 int is_digit(char *s)
 {
 	int i = 0;
+
 	while (s[i])
 	{
 		if (s[i] < '0' || s[i] > '9')
-	return (0);
+			return (0);
 		i++;
-	}
+}
 	return (1);
 }
+
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
@@ -28,12 +32,14 @@ int is_digit(char *s)
 int _strlen(char *s)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
-}
+		}
 	return (i);
 }
+
 /**
  * errors - handles errors for main
  */
@@ -42,6 +48,7 @@ void errors(void)
 	printf("Error\n");
 	exit(98);
 }
+
 /**
  * main - multiplies two positive numbers
  * @argc: number of arguments
@@ -53,6 +60,7 @@ int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
@@ -68,7 +76,7 @@ int main(int argc, char *argv[])
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
@@ -83,11 +91,11 @@ for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		if (result[i])
 			a = 1;
 		if (a)
-		_putchar(result[i] + '0');
+			_putchar(result[i] + '0');
 	}
 	if (!a)
 		_putchar('0');
-		_putchar('\n');
+	_putchar('\n');
 	free(result);
 	return (0);
 }
